c4185944d46bed4e4f22a5717136a96730046608
gregory <gregoryseminari@gmail.com>
2025-01-13 19:54:57 +0800

    update

--- resources/views/layouts_enduser/footer.blade.php
+++ resources/views/layouts_enduser/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-primary text-center py-6">
     <div class="container">
-        <p class="text-uppercase text-300 ls mb-3">Indoraya Akrilik, Bali, Indonesia</p>
+        <p class="text-uppercase text-300 ls mb-3">Dicky Laser Akrilik, Bali, Indonesia</p>
         <!-- <p class="text-700 mb-0">Template by <a class="text-600" href="https://themewagon.com/" target="_blank">Themewagon</a></p> -->
     </div>
 </footer>
