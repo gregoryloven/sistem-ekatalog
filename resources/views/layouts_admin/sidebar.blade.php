a8f560be15a109a15eefba77a191cb09a91d9b53
gregoryloven <gregoryseminari@gmail.com>
2025-02-17 09:14:26 +0800

    sidebar admin

--- resources/views/layouts_admin/sidebar.blade.php
+++ resources/views/layouts_admin/sidebar.blade.php
@@ -6,6 +6,7 @@
             <ul class="sidebar-menu">
                 <li><a class="nav-link" href="/"><i class="fas fa-fire"></i> <span>Home</span></a></li>
                 <li><a class="nav-link" href="/product"><i class="fas fa-list-alt"></i> <span>Produk</span></a></li>
+                <li><a class="nav-link" href="/purchase-request"><i class="fas fa-shopping-cart"></i> <span>Pesanan</span></a></li>
                 <li><a class="nav-link" href="/payment"><i class="fas fa-money-bill"></i> <span>Bukti Bayar</span></a></li>
             </ul>
             
